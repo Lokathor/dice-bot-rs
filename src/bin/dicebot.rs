0065f79b30318f0b9b80d30b5953d0ab21a13607
Lokathor <zefria@gmail.com>
2018-08-04 21:00:59 -0600

    updates

--- src/bin/dicebot.rs
+++ src/bin/dicebot.rs
@@ -40,64 +40,96 @@ fn main() {
           .delimiter(" ")
           .case_insensitivity(true)
       })
-      .command("as", |c| c.cmd(after_sundown))
-      .command("sr", |c| c.cmd(shadowrun)),
+      .simple_bucket("help", 30)
+      .command("echo", |c| c.check(owner_check).cmd(echo).desc("Admin-only echo test"))
+      .command("as", |c| {
+        c.cmd(after_sundown)
+          .desc("Rolls After Sundown style (max of 5 rolls)")
+          .usage("[DICE_COUNT] [...]")
+      })
+      .command("sr", |c| {
+        c.cmd(shadowrun)
+          .desc("Rolls Shadowrun 4e style (max of 5 rolls)")
+          .usage("[DICE_COUNT] [...]")
+      })
+      .command("dice", |c| {
+        c.cmd(dice)
+          .desc("Rolls a standard dice expression (max of 5 rolls)")
+          .usage("[DICE_EXPRESSION] [...]")
+      })
+      .help(help_commands::with_embeds),
   );
 
   if let Err(why) = client.start() {
-    println!("Client error: {:?}", why);
+    println!("Client::start error: {:?}", why);
   }
 }
 
+fn owner_check(_: &mut Context, msg: &Message, _: &mut Args, _: &CommandOptions) -> bool {
+  msg.author.id == LOKATHOR_ID
+}
+
+command!(echo(_ctx, msg, args) {
+  msg.channel_id.say(format!("{}",args.full())).ok();
+});
+
 command!(after_sundown(_ctx, msg, args) {
-  match args.single::<u32>() {
-    Ok(dice_count) => {
-      //msg.react(ReactionType::Unicode(EMOJI_CHECK.to_string())).ok();
-      let gen: &mut PCG32 = &mut get_global_generator();
-      let mut hits = 0;
-      for _ in 0 .. dice_count {
-        if d6.sample_with(gen) > 4 {
-          hits += 1;
+  let gen: &mut PCG32 = &mut get_global_generator();
+  for arg in args.iter::<u32>().take(5) {
+    match arg {
+      Ok(dice_count) => {
+        let mut hits = 0;
+        for _ in 0 .. dice_count.min(5_000) {
+          if d6.sample_with(gen) > 4 {
+            hits += 1;
+          }
         }
+        if let Err(why) = msg.channel_id.say(format!("Rolled {} dice, got {} hit{}", dice_count, hits, if hits != 1 {"s"} else {""})) {
+          println!("Error sending message: {:?}", why);
+        }
+      },
+      Err(_) => {
+        msg.react(ReactionType::Unicode(EMOJI_QUESTION.to_string())).ok();
       }
-      if let Err(why) = msg.channel_id.say(format!("Rolled {} dice, got {} hit{}", dice_count, hits, if hits != 1 {"s"} else {""})) {
-        println!("Error sending message: {:?}", why);
-      }
-    },
-    Err(_) => {
-      msg.react(ReactionType::Unicode(EMOJI_QUESTION.to_string())).ok();
     }
   }
 });
 
 command!(shadowrun(_ctx, msg, args) {
-  match args.single::<u32>() {
-    Ok(dice_count) => {
-      //msg.react(ReactionType::Unicode(EMOJI_CHECK.to_string())).ok();
-      let gen: &mut PCG32 = &mut get_global_generator();
-      let mut hits = 0;
-      let mut ones = 0;
-      for _ in 0 .. dice_count {
-        let roll = d6.sample_with(gen);
-        if roll == 1 {
-          ones += 1;
-        } else if roll > 4 {
-          hits += 1;
+  let gen: &mut PCG32 = &mut get_global_generator();
+  for arg in args.iter::<u32>().take(5) {
+    match arg {
+      Ok(dice_count) => {
+        let mut hits = 0;
+        let mut ones = 0;
+        for _ in 0 .. dice_count.min(5_000) {
+          let roll = d6.sample_with(gen);
+          if roll == 1 {
+            ones += 1;
+          } else if roll > 4 {
+            hits += 1;
+          }
         }
+        let is_glitch = ones >= (dice_count+1) / 2;
+        let output = format!("Rolled {} dice: {}{} hit{}", dice_count, match (hits, is_glitch) {
+          (0, true) => "CRITICAL GLITCH, ",
+          (_, true) => "GLITCH, ",
+          _ => "",
+        }, hits, if hits != 1 {"s"} else {""});
+        if let Err(why) = msg.channel_id.say(output) {
+          println!("Error sending message: {:?}", why);
+        }
+      },
+      Err(_) => {
+        msg.react(ReactionType::Unicode(EMOJI_QUESTION.to_string())).ok();
       }
-      let is_glitch = ones >= (dice_count+1) / 2;
-      let output = format!("Rolled {} dice: {}, {} hit{}", dice_count, match (hits, is_glitch) {
-        (0, true) => "CRITICAL GLITCH",
-        (_, true) => "GLITCH",
-        (0, false) => "failure",
-        (_, false) => "success",
-      }, hits, if hits != 1 {"s"} else {""});
-      if let Err(why) = msg.channel_id.say(output) {
-        println!("Error sending message: {:?}", why);
-      }
-    },
-    Err(_) => {
-      msg.react(ReactionType::Unicode(EMOJI_QUESTION.to_string())).ok();
     }
+  }
+});
+
+command!(dice(_ctx, msg, args) {
+  let gen: &mut PCG32 = &mut get_global_generator();
+  for arg_str in args.iter::<&str>().take(5) {
+
   }
 });
