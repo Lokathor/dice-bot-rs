e0c1ee2706babca5d6fa0bed5ee459c4bf6f266f
Lokathor <zefria@gmail.com>
2020-02-09 14:48:39 -0700

    seed the generator properly at starup.

--- src/global_gen.rs
+++ src/global_gen.rs
@@ -11,7 +11,10 @@ static M: AtomicPtr<Mutex<PCG32>> = AtomicPtr::new(null_mut());
 pub fn global_gen() -> MutexGuard<'static, PCG32> {
   let p: *mut Mutex<PCG32> = M.load(Ordering::SeqCst);
   if p.is_null() {
-    let b = Box::new(Mutex::new(PCG32::default()));
+    let mut arr = [0_u64, 0];
+    getrandom::getrandom(bytemuck::bytes_of_mut(&mut arr)).unwrap();
+    let [state, inc] = arr;
+    let b = Box::new(Mutex::new(PCG32::seed(state, inc)));
     let new_p: *mut Mutex<PCG32> = Box::leak(b);
     if M.compare_and_swap(null_mut(), new_p, Ordering::SeqCst).is_null() {
       // success: leave the mutex in a leaked state
